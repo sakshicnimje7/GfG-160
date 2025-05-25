86ce9d6a1239c51c371d3f538407ac67dc20f543
Sakshi Nimje <sakshinimje7@gmail.com>
2025-05-25 15:09:38 +0530

    Arrays
    Iterate over the given array from end and find the first index (pivot) which doesn't follow property of non-increasing suffix, (i.e,  arr[i] < arr[i + 1]).
    If pivot index does not exist, then the given sequence in the array is the largest as possible. So, reverse the complete array. For example, for [3, 2, 1], the output would be [1, 2, 3]
    Otherwise, Iterate the array from the end and find for the successor (rightmost greater element) of pivot in suffix.
    Swap the pivot and successor
    Minimize the suffix part by reversing the array from pivot + 1 till n.
    
    Time Complexity: O(n), where n is the size of the given array.
    Auxiliary Space: O(1), The algorithm performs in-place operations (modifying the array directly) without using extra space proportional to the input size

---
+++ Arrays/NextPermutation.java
@@ -0,0 +1,43 @@
+// User function Template for Java
+
+class Solution5 {
+    static void nextPermutation(int[] arr) {
+        
+        int n = arr.length;
+        int pivot = -1;
+        for (int i = n - 2; i >= 0; i--) {
+            if (arr[i] < arr[i+1]) {
+                pivot = i;
+                break;
+            }
+        }
+        if (pivot == -1) {
+            reverse(arr, 0, n - 1);
+            return;
+        }
+        for (int i = n - 1; i > pivot; i--) {
+            if (arr[i] > arr[pivot]) {
+                swap(arr, i, pivot);
+                break;
+            }
+        }
+        reverse(arr, pivot+1, n - 1);
+    }
+    private static void reverse(int[] arr, int start, int end) {
+        while(start < end) {
+            swap(arr, start++, end--);
+        }
+    }
+    private static void swap(int[] arr, int i, int j) {
+        int temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    }
+    public static void main(String[] args) {
+        int[] arr = {2, 4, 1, 7, 5, 0};
+        nextPermutation(arr);
+        
+        for(int i = 0; i < arr.length; i++)
+        System.out.print(arr[i] + " ");
+    }
+}
